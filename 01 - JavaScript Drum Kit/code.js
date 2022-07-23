b0f4968af94717ddb7f1505f5b1cf824a68bd905
Dario Cruz <rio2012@gmail.com>
2022-07-23 12:47:33 -0400

    Add evenlistening code. Link to css animation not working.

--- 01 - JavaScript Drum Kit/code.js
+++ 01 - JavaScript Drum Kit/code.js
@@ -1,9 +1,21 @@
 // Code for registering key input.
 // VScode is stating that keyCode is depricated.
 // Will have to look into the non-depricated method.
+
 window.addEventListener('keydown', function(e) {
-    // Code for logging that audio file is tied to key.
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(audio);
-    console.log (e.keyCode);
+    const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
+    if (!audio) return; // stops function.
+    audio.play(); // plays the associated audio.
+    audio.currentTime = 0; // returns audio to start on press.
+    key.classList.add('playing'); // Links to css animation property.
+
 });
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+    // console.log(e.propertyName);
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach (key => key.addEventListener('transitionend', removeTransition));
