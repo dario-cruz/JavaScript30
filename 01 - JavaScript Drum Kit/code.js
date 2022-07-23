8736b68dc7a47dc5ef72be58f460d6c1a269d9bb
Dario Cruz <rio2012@gmail.com>
2022-07-23 12:09:12 -0400

    Add js code for logging audio tied to key.

--- 01 - JavaScript Drum Kit/code.js
+++ 01 - JavaScript Drum Kit/code.js
@@ -2,5 +2,8 @@
 // VScode is stating that keyCode is depricated.
 // Will have to look into the non-depricated method.
 window.addEventListener('keydown', function(e) {
+    // Code for logging that audio file is tied to key.
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    console.log(audio);
     console.log (e.keyCode);
 });
