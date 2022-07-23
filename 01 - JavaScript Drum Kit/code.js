0ebc14de27dd58c79d815ebf0e4afe7fb53980fe
Dario Cruz <rio2012@gmail.com>
2022-07-23 12:02:40 -0400

    Add function for identifing key inputs.

---
+++ 01 - JavaScript Drum Kit/code.js
@@ -0,0 +1,6 @@
+// Code for registering key input.
+// VScode is stating that keyCode is depricated.
+// Will have to look into the non-depricated method.
+window.addEventListener('keydown', function(e) {
+    console.log (e.keyCode);
+});
